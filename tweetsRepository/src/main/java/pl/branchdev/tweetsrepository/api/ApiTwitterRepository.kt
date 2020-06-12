251470fc58b388bb4e782907f676fe263f209f64
Patryk Gałązka <patrykgalazka96@gmail.com>
2020-06-12 15:21:16 +0200

    refactor configuration and readme added

--- tweetsRepository/src/main/java/pl/branchdev/tweetsrepository/api/ApiTwitterRepository.kt
+++ tweetsRepository/src/main/java/pl/branchdev/tweetsrepository/api/ApiTwitterRepository.kt
@@ -1,10 +1,10 @@
 package pl.branchdev.tweetsrepository.api
 
-import pl.branchdev.data.TweetDto
 import com.google.gson.Gson
 import io.reactivex.Observable
 import okio.BufferedSource
 import pl.branchdev.common.rx.SchedulerProvider
+import pl.branchdev.data.TweetDto
 import pl.branchdev.tweetsrepository.TwitterRepository
 import retrofit2.HttpException
 import java.io.IOException
@@ -22,12 +22,15 @@ class ApiTwitterRepository(
             .flatMap { mapResponseBodyToStringObservable(it.source()) }
             .retryWhen { throwable ->
                 throwable.flatMap {
-                    if (it is SocketTimeoutException || it is HttpException)
+                    if (shouldReconnect(it))
                         return@flatMap Observable.just(Object())
                     else
                         return@flatMap Observable.error<Throwable> { it }
                 }
             }.map { gson.fromJson(it, TweetDto::class.java) }
+
+    private fun shouldReconnect(error: Throwable) =
+        error is SocketTimeoutException || error is HttpException
 
 
     private fun mapResponseBodyToStringObservable(source: BufferedSource): Observable<String> {
