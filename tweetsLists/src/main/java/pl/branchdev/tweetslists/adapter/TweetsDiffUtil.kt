ae7514d960d44bd4ba74caf1a7f95ec020eda9ef
Patryk Gałązka <patrykgalazka96@gmail.com>
2020-06-12 15:33:10 +0200

    fix endpoint configuration and code style issues

--- tweetsLists/src/main/java/pl/branchdev/tweetslists/adapter/TweetsDiffUtil.kt
+++ tweetsLists/src/main/java/pl/branchdev/tweetslists/adapter/TweetsDiffUtil.kt
@@ -12,7 +12,6 @@ class TweetsDiffUtil(
     override fun getNewListSize() = newList.size
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
         oldList[oldItemPosition].id == newList[newItemPosition].id
-
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) =
         oldList[oldItemPosition].text == newList[newItemPosition].text
 }
--- tweetsRepository/src/main/java/pl/branchdev/tweetsrepository/api/ApiEndpointsConfiguration.kt
+++ tweetsRepository/src/main/java/pl/branchdev/tweetsrepository/api/ApiEndpointsConfiguration.kt
@@ -1,5 +1,5 @@
 package pl.branchdev.tweetsrepository.api
 
 object ApiEndpointsConfiguration{
-    const val statusesStreamUrl = "statuses/filter.json?track=Michael Jo"
+    const val statusesStreamUrl = "statuses/filter.json"
 }
